8b18c9f09c3ac3f5548056aaf55044299ea95a1c
Denis <47417617+Denis412@users.noreply.github.com>
2023-10-15 20:22:36 +0300

    init state the project

--- src/app/vite-env.d.ts
+++ src/app/vite-env.d.ts
@@ -1,5 +1,19 @@
 /// <reference types="vite/client" />
 
+declare module '@app';
+declare module '@processes';
+declare module '@layouts';
+declare module '@pages';
+declare module '@widgets';
+declare module '@features';
+declare module '@entities';
+declare module '@shared';
+
+declare module '*.scss' {
+    const content: { [className: string]: string };
+    export = content;
+}
+
 declare module '*.vue' {
     import { DefineComponent } from 'vue';
     // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
